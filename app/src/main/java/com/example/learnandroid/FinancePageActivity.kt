521af34e9816351826aad01e00f7a76e1aa0b9bb
lucky_2206 <vaibhavjainlucky98@gmail.com>
2024-01-07 02:14:42 +0530

    Finance UI added using constrain layout

---
+++ app/src/main/java/com/example/learnandroid/FinancePageActivity.kt
@@ -0,0 +1,11 @@
+package com.example.learnandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FinancePageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_finance_page)
+    }
+}
